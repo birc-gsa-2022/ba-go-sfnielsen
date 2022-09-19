8eea64518d754a1f4fd5f39cba87e5ff6b6ae730
Simon <au649801@uni.au.dk>
2022-09-19 18:50:57 +0200

    greasy fix for strict ba that seems to work

--- gsa/ba.go
+++ gsa/ba.go
@@ -35,33 +35,42 @@ func StrictBorderarray(x string) []int {
 	b := 0
 	i := 1
 	for i < len(x) {
-		if x[b] == x[i] {
+
+		//we have a border
+		if x[i] == x[b] {
 			b++
-			if i == len(x)-1 {
-				ba[i] = b
-			}
 			i++
-			continue
+			//case where last index element is a border
+			if i == len(x) {
+				ba[len(x)-1] = b
+				break
+			}
+			//found border is not strict
+			if x[i] == x[b] {
+				if b-2 >= 0 {
+					b_2 := ba[b-1]
+					//while loop to find greatest strict border (if exists)
+					for b_2 > 0 {
+						if x[i-1] == x[b_2-1] {
+							if x[i] != x[b_2] {
+								ba[i-1] = b_2
+								break
+							}
+						}
+						b_2 = ba[b_2-1]
+					}
+				}
+				//case where current index was a strict border
+			} else {
+				ba[i-1] = b
+				b = 0
+			}
+			//case where current index is not a border
+		} else {
+			i++
+			b = 0
 		}
-		if ba[i-1] == 0 {
-			ba[i-1] = b
-		}
-		if b > 0 {
-			b = ba[b-1]
-			continue
-		}
-		i++
 	}
 	return ba
 
-	/* **simpler but 2*n solution***
-
-	ba = Borderarray(x)
-	for i := 1; i < len(ba); i++ {
-		if ba[i-1] == ba[i]-1 {
-			ba[i-1] = 0
-		}
-	}
-	return ba
-	*/
 }
